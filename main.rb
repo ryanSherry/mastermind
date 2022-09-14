f4a9d64f9baea8fc50c55e33e99d96622f5a8b81
ryanSherry <rsherrycomputers@gmail.com>
2022-09-14 16:40:43 -0700

    pseudocode and gitignore

---
+++ main.rb
@@ -0,0 +1,9 @@
+class Main
+  # print codemaker instructions and choices
+  # accept codemaker code
+  # print codebreaker instructions and choices
+  # accept guess
+  # give feedback on guess
+  # increment turn and repeat for 9 turns or until win
+  # print past turns with feedback
+end
